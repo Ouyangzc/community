61537a4fee74c9c1aae35b274b577f14feb16ee4
oyang <oyangzc@163.com>
2019-07-02 22:53:36 +0800

    add session cookie

---
+++ src/main/java/com/ouyang/community/controller/OAuthController.java
@@ -0,0 +1,63 @@
+package com.ouyang.community.controller;
+
+import com.ouyang.community.dto.AccessTokenDTO;
+import com.ouyang.community.dto.GithubUser;
+import com.ouyang.community.mapper.UserMapper;
+import com.ouyang.community.model.User;
+import com.ouyang.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
+
+@Controller
+public class OAuthController {
+    @Autowired
+    private GithubProvider provider;
+    @Autowired
+    private UserMapper userMapper;
+
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
+    @GetMapping("/callback")
+    public String callBack(@RequestParam(name = "code") String code,
+                           @RequestParam(name="state") String state,
+                           HttpServletRequest request,
+                           HttpServletResponse response){
+
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        accessTokenDTO.setState(state);
+        String accessToken = provider.getAccessToke(accessTokenDTO);
+        GithubUser githubUser = provider.getUser(accessToken);
+        if(githubUser !=  null ){
+            User user = new User();
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setName(githubUser.getName());
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            //登陆成功绑定token
+            response.addCookie(new Cookie("token",token));
+            return  "redirect:/";
+        }else{
+            return  "redirect:/";
+        }
+    }
+}
